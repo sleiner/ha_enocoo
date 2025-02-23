356c22e876261e5270fd2cdd27c60cd59651020c
Simon Leiner <simon@leiner.me>
2025-02-23 18:04:32 +0100

    Insert Individual consumption statistics

--- custom_components/ha_enocoo/_util.py
+++ custom_components/ha_enocoo/_util.py
@@ -1,6 +1,9 @@
 """Various utilities for other modules."""
 
-from collections.abc import Callable, Coroutine, Sequence
+import datetime as dt
+from collections.abc import Callable, Coroutine, Generator, Sequence
+
+from oocone.model import Consumption, PhotovoltaicSummary
 
 
 async def bisect[T](
@@ -34,3 +37,25 @@ async def bisect[T](
         raise StopIteration(msg)
 
     return lo
+
+
+MeasurementWithinPeriod = Consumption | PhotovoltaicSummary
+
+
+def zip_measurements[T: MeasurementWithinPeriod, U: MeasurementWithinPeriod](
+    measurement_series_1: Sequence[T],
+    measurement_series_2: Sequence[U],
+    *,
+    strict: bool = False,
+) -> Generator[tuple[dt.datetime, dt.timedelta, T, U]]:
+    for m1, m2 in zip(measurement_series_1, measurement_series_2, strict=strict):
+        if m1.start != m2.start:
+            msg = f"Start dates {m1.start} and {m2.start} do not match."
+            raise ValueError(msg)
+        if m1.period != m2.period:
+            msg = (
+                f"Periods {m1.period} and {m2.period} starting from {m1.start}"
+                " do not match."
+            )
+            raise ValueError(msg)
+        yield (m1.start, m1.period, m1, m2)
